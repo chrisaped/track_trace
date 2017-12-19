b5a9f28aaa23fbdd238dcb293f97b102b5b3bd7f
Christopher Pedersen <chrisaped@gmail.com>
2017-12-19 11:11:25 -0500

    rails basic setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'bookings#index'
+
+  get '/bookings/:id', to: 'bookings#show', as: 'booking'
+  get '/search', to: 'bookings#search', as: 'search'
 end
