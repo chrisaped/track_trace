ad0866af5b3e3c50802dc93764e04a5c0d7889bf
Christopher Pedersen <chrisaped@gmail.com>
2017-12-19 19:16:40 -0500

    working!

--- client/src/BookingSearch.js
+++ client/src/BookingSearch.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import Client from './Client';
 
 class BookingSearch extends React.Component {
 	state = {
 		searchValue: '',
+		searchResult: '',
+		status: '',
 	};
+
+	// rename this
+	checkStatus = (response) => {
+	  this.setState({
+			status: response.status,
+		});
+
+		return response;
+	}
+
+	parseJSON = (response) => {
+	  return response.json();
+	}	
+
+	search = (query) => {
+	  return fetch(`bookings/${query}`, {
+	    accept: 'application/json',
+	  }).then(this.checkStatus)
+	    .then(this.parseJSON)
+	    .then(result => this.setState({ searchResult: result }));
+	}	
 
 	updateSearchValue = (e) => {
 		const value = e.target.value;
@@ -17,25 +39,47 @@ class BookingSearch extends React.Component {
 	searchBookings = () => {
 		const searchValue = this.state.searchValue;
 
-		if (searchValue === '') {
-			console.log('nothing here!');
+		if (searchValue) {
+			this.search(searchValue);
 		} else {
-			Client.search(searchValue);
+			window.alert("Please enter a value!");
 		}
 	}
 
+
 	render() {
+		const {
+			searchResult,
+			status,
+		} = this.state;
+
+		console.log('FUCKER', searchResult);
+		console.log("STATUS", status);
+
+		let searchResultDOM = null;
+
+		if (status === 200) {
+		  searchResultDOM = <p>{searchResult.booking_number}</p>;
+		} else if (status === 404) {
+			searchResultDOM = <p>ERROR</p>;
+		}
+
 		return (
-			<div id='booking-search'>
-				<input
-					type='text'
-					placeholder='Enter a booking number'
-					value={this.state.searchValue}
-					onChange={this.updateSearchValue}
-				/>
-				<button onClick={this.searchBookings}>
-			    Search
-			  </button>
+			<div>
+				<div id='booking-search'>
+					<input
+						type='text'
+						placeholder='Enter a booking number'
+						value={this.state.searchValue}
+						onChange={this.updateSearchValue}
+					/>
+					<button onClick={this.searchBookings}>
+				    Search
+				  </button>
+				</div>
+				<div>
+					{searchResultDOM}
+				</div>
 			</div>
 		);
 	}
